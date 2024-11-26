02bd576f9e2bc5a880c387d6a87208490423b986
Ángela <angelasaezgim12@gmail.com>
2024-11-26 20:53:22 +0100

    Actualizaci├│n autom├ítica desde script

--- UD4-Kotlin/DiceAngela/src/Main.kt
+++ UD4-Kotlin/DiceAngela/src/Main.kt
@@ -5,27 +5,62 @@
  * @author Angela
  * @version 1.0
  */
-/*
-Ahora en el main se deben realizar las siguientes acciones:
-- Pedir al usuario el número de caras del dado entre 4 y 20 y crear un dado con ese número de caras.
-- Realizar una tirada del dado y mostrar el valor obtenido
-y a continuación, preguntar al usuario si piensa que va a salir un número mayor o menor.
-- Repetir las tiradas mientras el usuario acierte (mayor o menor que la anterior).
-Ejemplo:
-- En la 1ª tirada sale un 12
-- El usuario dice mayor
-- Se hace una 2ª tirada y sale un 19
-- Se vuelve a preguntar al usuario y dice mayor
-- Se hace una 3ª tirada y sale 4.
-- Se acaba el juego.
-Cuando se acabe el juego se debe mostrar la información del dado usado (usando su to_string).
-*/
+
 fun main() {
 
-    println("Hola, vamos a simular lanzamientos de dados");
+    println("Hola, vamos a simular lanzamientos de dados")
 
-    println("En primer lugar, dime cuantas caras tiene el dado (entre 4 y 20)")
-    var inputNumFaces= readlnOrNull()
+    val numFaces = validateNumFaces()
+    val dice = Dice(numFaces)
 
+    println("Dado creado con ${dice.faces} caras, realizamos una tirada")
+    // Primera tirada
+    var numberThrow = dice.throwDice()
+    println("Primera tirada, valor obtenido : $numberThrow")
+
+    var correctAnswer: Boolean = false
+    do {
+        println("¿Piensas que la siguiente tirada será 'mayor' o 'menor'?")
+        val guessInput = readlnOrNull()?.lowercase()
+
+        if (guessInput != "mayor" && guessInput != "menor") {
+            println("Respuesta no válida. Por favor, escribe 'mayor' o 'menor'.")
+            continue
+        }
+
+        // Nueva tirada
+        val numberThrow2 = dice.throwDice()
+        println("Valor obtenido: $numberThrow2")
+
+        //correctAnswer se pondrá en true si el input y la condición se cumplen (si es MAYOR Y es eso) O (si es MENOR Y es eso)
+        correctAnswer = (guessInput == "mayor" && numberThrow2 > numberThrow ) ||
+                        (guessInput == "menor" && numberThrow2 < numberThrow)
+
+        if (correctAnswer) {
+            println("¡Correcto! Salimos.")
+        } else {
+            println("¡Has fallado! Continua")
+            numberThrow = numberThrow2 //Actualizamos para que el siguiente numero a revisar sea el ultimo que hemos puesto
+        }
+    } while (!correctAnswer)
+
+    println("Felicidades! Ganaste \nEsta es la información de tus tiradas ; \n ----------- \n $dice. \n ----------- ")
+
+}
+
+/**
+ * Función que valida que hemos introducido correctamente el numero de caras en los límites establecidos
+ * @return numero de caras del dado
+ */
+
+fun validateNumFaces(): Int {
+    var inputNumFaces: Int = 0
+    do {
+        println("En primer lugar, dime cuantas caras tiene el dado (entre 4 y 20)")
+        inputNumFaces = readlnOrNull()?.toInt()!!
+        if (inputNumFaces < 4 || inputNumFaces > 20) println("Número no válido, debe ser entre 4 y 20")
+    } while (inputNumFaces < 4 || inputNumFaces > 20)
+
+    return inputNumFaces;
 }
 
